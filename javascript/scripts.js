362e0f486509e9c512a39ddcdfaa0f55ff8962eb
Ashley Savage <chavon1189@gmail.com>
2021-09-17 10:08:39 -0400

    added tic tac toe

---
+++ javascript/scripts.js
@@ -0,0 +1,9 @@
+//This is a  single line comment//
+/**
+ * this is a multi-line comment
+ * this is also a multi-line comment.
+ * I can just keep going like this!
+ */
+
+/* also a multi-line comment */
+console.log("How are you?")
