22e33b45ea0f75239b24c908a481425a8c8f185c
Ashley Savage <chavon1189@gmail.com>
2021-09-21 09:36:28 -0400

    completes banner

--- javascript/banner.js
+++ javascript/banner.js
@@ -1,4 +1,10 @@
-function printBanner(meessage){
-    for
+function printBanner(message){
+    console.log("-".repeat(message.length))
+    for (let i = 0; i < 1; i++) {
+        console.log("-" + message + "-")
+    }
+    console.log("_".repeat(message.length))
 
 }
+
+printBanner("Welcome to Digital Crafts!")
