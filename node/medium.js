f3520ce2e25a6dd30d40aeb3bb1fb04dc0529a6c
Ashley Savage <chavon1189@gmail.com>
2021-10-12 10:52:16 -0400

    completes first medium exercise

---
+++ node/medium.js
@@ -0,0 +1,16 @@
+const dns = require("dns")
+const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+readline.question("Domain Name ", function(url){
+    readline.close();
+    dns.lookup(url, function(err, address){
+        if (err){
+            console.log(err.message);
+            return;
+        }
+        console.log("IP Address: ", address);
+    })
+})
